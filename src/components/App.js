6efbe20bfcaccf435b77a6b4f7a567e29f53c1c7
Tyler Metevier <tyler.metevier@gmail.com>
2017-09-16 21:25:53 -0400

    Add initial webpack config including React, Jest, SCSS, and Babel; Add hot-reloading;

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '@scss/App';
+
+const App = () => {
+  return (
+    <div className='app'>VIA To-Do</div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3000;
+
+module.exports = {
+  entry: {
+    app: [
+      'react-hot-loader/patch',
+      `webpack-dev-server/client?http://${HOST}:${PORT}`,
+      'webpack/hot/only-dev-server',
+      './src/index.js'
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'cheap-source-map',
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src', 'components'),
+      '@scss': path.resolve(__dirname, 'src', 'scss')
+    },
+    extensions: ['.js', '.scss']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    host: HOST,
+    port: PORT,
+    hot: true
+  },
+};
